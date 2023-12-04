d1b714b2963a5901b289058ec669cece3795084b
Gargi Tawde <gargi.tawde01@gmail.com>
2023-12-03 21:33:02 -0800

    Basic Powerball and Mega Millions windows; users can select dates and retrieve winning number information based on selection

--- home_view.py
+++ home_view.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from tkinter import *
 from tkinter import ttk
 
@@ -20,7 +21,7 @@ class HomeView:
     def __init__(self, root):
         
         root.title("New York Lottery Analysis Home View")
-        root.geometry('280x300')
+        root.geometry('525x350')
 
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
@@ -43,32 +44,24 @@ class HomeView:
         self.button_new_window = ttk.Button(self.frame, text="MEGA MILLIONS", command=self.mega_millions_window)
         self.button_new_window.grid(column=0, row=2, sticky=(N, W, E, S))
 
+        # add a button to close the app
+        self.button_close = ttk.Button(self.frame,
+                                          text="EXIT", command=root.destroy)
+        self.button_close.grid(column=0, row=3, sticky=(N, W, E, S))
+        
+    # How to use different python files
+    # def test(self):
+    #     os.system('python3 test.py')
 
     # POWERBALL Window
     def powerball_window(self):
-        win = Toplevel()
-        win.wm_title('Powerball Window')
-        
-        label = ttk.Label(win, text="New widgets go here")
-        label.grid(row=0, column=0)
-
-        self.button_close = ttk.Button(win,
-        text="Close", command=win.destroy)
-        self.button_close.grid(column=0, row=5, sticky=(N, W, E, S))
+        os.system('python3 powerball.py')
 
     # MEGA MILLIONS WINDOW
     def mega_millions_window(self):
-        win = Toplevel()
-        win.wm_title('Mega Millions Window')
+        os.system('python3 mega_millions.py')
+
         
-        label = ttk.Label(win, text="New widgets go here")
-        label.grid(row=0, column=0)
-
-        self.button_close = ttk.Button(win,
-        text="Close", command=win.destroy)
-        self.button_close.grid(column=0, row=5, sticky=(N, W, E, S))
-
-
 
 
 root = Tk()
